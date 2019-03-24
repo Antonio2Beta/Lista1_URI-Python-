4c08771767a64af449d60e73624e2aeaddb4397b
Antonio2Beta <38213571+Antonio2Beta@users.noreply.github.com>
2019-03-24 19:46:01 -0300

    Create 1038.py

---
+++ 1038.py
@@ -0,0 +1,14 @@
+val = input().split(" ")
+x, y = val
+x = int(x)
+y = int(y)
+if (x==1):
+  print('Total: R$ {:.2f}'.format(y*a))
+if (x==2):
+  print('Total: R$ {:.2f}'.format(y*4.5))
+if (x==3):
+  print('Total: R$ {:.2f}'.format(y*5))
+if (x==4):
+  print('Total: R$ {:.2f}'.format(y*2))
+if (x==5):
+  print('Total: R$ {:.2f}'.format(y*1.5))
