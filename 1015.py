e24e0c84424a059a5886e788a019157cc4d0d5ae
Antonio2Beta <38213571+Antonio2Beta@users.noreply.github.com>
2019-04-07 23:14:02 -0300

    Update 1015.py

--- 1015.py
+++ 1015.py
@@ -1,11 +1,9 @@
 from math import sqrt as raiz
 
-val = input().split(" ")
-x1, y1 = val
+x1, y1 = input().split(" ")
 x1 = float(x1)
 y1 = float(y1)
-val2 = input().split(" ")
-x2, y2 = val2
+x2, y2 = input().split(" ")
 x2 = float(x2)
 y2 = float(y2)
 
